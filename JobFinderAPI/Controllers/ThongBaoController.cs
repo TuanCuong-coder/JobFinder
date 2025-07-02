31892f563825be418444a86cda710a54b7e632b5
chithanh <chipngaocho123@gmail.com>
2025-07-02 23:37:17 +0700

    Chuc nang thong bao tu dong

--- JobFinderAPI/Controllers/ThongBaoController.cs
+++ JobFinderAPI/Controllers/ThongBaoController.cs
@@ -17,6 +17,29 @@ namespace JobFinderAPI.Controllers
             _context = context;
         }
 
+        // Lấy danh sách thông báo cho ứng viên (loại: 'xem_cv')
+        [HttpGet("UngVien")]
+        [Authorize]
+        public async Task<IActionResult> LayThongBaoUngVien()
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var thongBao = await _context.ThongBaos
+                .Where(tb => tb.NguoiNhanId == userId && tb.Loai == "xem_cv")
+                .OrderByDescending(tb => tb.ThoiGian)
+                .Select(tb => new
+                {
+                    tb.Id,
+                    tb.NoiDung,
+                    tb.Loai,
+                    tb.TrangThai,
+                    tb.ThoiGian
+                })
+                .ToListAsync();
+
+            return Ok(thongBao);
+        }
+
         // Lấy danh sách thông báo cho nhà tuyển dụng (loại: 'nop_cv')
         [HttpGet("NhaTuyenDung")]
         [Authorize]
@@ -32,32 +55,14 @@ namespace JobFinderAPI.Controllers
                     tb.Id,
                     tb.NoiDung,
                     tb.Loai,
+                    tb.TrangThai,
+                    tb.ThoiGian
                 })
                 .ToListAsync();
 
             return Ok(thongBao);
         }
 
-        // Lấy danh sách thông báo cho ứng viên (loại: 'xem_cv')
-        [HttpGet("UngVien")]
-        [Authorize]
-        public async Task<IActionResult> LayThongBaoUngVien()
-        {
-            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-
-            var thongBao = await _context.ThongBaos
-                .Where(tb => tb.NguoiNhanId == userId && tb.Loai == "xem_cv")
-                .OrderByDescending(tb => tb.ThoiGian)
-                .Select(tb => new
-                {
-                    tb.Id,
-                    tb.NoiDung
-                })
-                .ToListAsync();
-
-            return Ok(thongBao);
-        }
-     
         // Đánh dấu 1 thông báo là đã đọc
         [HttpPost("DanhDauDaDoc/{id}")]
         [Authorize]
@@ -66,6 +71,7 @@ namespace JobFinderAPI.Controllers
             var thongBao = await _context.ThongBaos.FindAsync(id);
             if (thongBao == null)
                 return NotFound(new { message = "Không tìm thấy thông báo" });
+
             thongBao.TrangThai = "da_doc";
             await _context.SaveChangesAsync();
 
@@ -78,8 +84,10 @@ namespace JobFinderAPI.Controllers
         public async Task<IActionResult> DemThongBaoChuaDoc()
         {
             var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
             var soLuong = await _context.ThongBaos
                 .CountAsync(tb => tb.NguoiNhanId == userId && tb.TrangThai == "chua_doc");
+
             return Ok(new { soLuong });
         }
     }
